461e5e577ada510cdf58a71d7428faed8d04cc75
RickFlinn <mriodine@gmail.com>
2019-01-17 23:36:27 -0800

    Added classes for all Objects in given JSON file, as well as a FeatureCollection class to hold them. Added JSONObjParser class that allows us to parse our JSON file for usable .NET objects.

---
+++ LINQ_In_Manhattan/LINQ_In_Manhattan/Classes/Feature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_In_Manhattan.Classes
+{
+
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+}
---
+++ LINQ_In_Manhattan/LINQ_In_Manhattan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using LINQ_In_Manhattan.Classes;
+
+
+namespace LINQ_In_Manhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string dataPath = "../../../data.json";
+            JSONObjParser builder = new JSONObjParser(dataPath);
+            FeatureCollection<Feature> collectiony = builder.DeSerializeFeatures(builder.ParseFileToString());
+
+            
+        }
+
+        
+    }
+}
